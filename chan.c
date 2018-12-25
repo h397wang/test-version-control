b96aabdc12cb47c354736763422a60631dcc6bda
Jackson Guo <guo_jackson@yahoo.ca>
2018-12-24 20:31:55 -0500

    added test

--- chan.c
+++ chan.c
@@ -19,7 +19,7 @@ int schan_init(struct schan *p)
 		return -1;
 
 	sem_init(&p->sem, 0, 0);
-	sem_destroy(&p->got);
+	sem_init(&p->got, 0, 0);
 	pthread_mutex_init(&p->mut, NULL);
 	p->box = NULL;
 
@@ -45,7 +45,7 @@ int schan_put(struct schan *p, void *msg)
 		return -1;
 
 	pthread_mutex_lock(&p->mut);
-	p->box = msg;
+	p->box = msg;	
 	pthread_mutex_unlock(&p->mut);
 	sem_post(&p->sem);
 	sem_wait(&p->got);
@@ -70,10 +70,13 @@ int schan_get(struct schan *p, void **res)
 void *run_receiver(void *chan)
 {
 	void *ptr;
-	schan_get(chan, &ptr);
-	printf("Got: %d\n", *((int *) ptr));
+	while (1) {
+		schan_get(chan, &ptr);
+		printf("Got: %d\n", *((int *) ptr));
+	}
 }
 
+/** Todo: Cleanup schan semaphores after cancel */
 int main()
 {
 	struct schan *chan = malloc(sizeof(struct schan));
@@ -81,8 +84,12 @@ int main()
 	pthread_create(&tid, NULL, run_receiver, chan);
 
 	int *msg = malloc(sizeof(int));
-	*msg = 1000;
-	schan_put(chan, msg);
+	for (int i=0; i<10; ++i) {
+		*msg = i;
+		schan_put(chan, msg);
+	}
+
+	pthread_cancel(tid);
 	pthread_join(tid, NULL);
 
 	free(msg);
